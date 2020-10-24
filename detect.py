764d962b3222e7c406c7f8166af7e3b03f2789f3
Promisery <37978140+Promisery@users.noreply.github.com>
2020-10-24 17:20:39 +0800

    Tried perspective transformation, still working on it

--- detect.py
+++ detect.py
@@ -5,14 +5,21 @@ import os
 import time
 
 
-def transform(image):
+def transform(image, reverse=False):
     height = image.shape[0]
     width = image.shape[1]
-    pts1 = np.float32([[330, 720], [566, 288], [1010, 720], [575, 288]])
-    pts2 = np.float32([[330, 720], [330, 288], [1010, 720], [1010, 288]])
+    if not reverse:
+        pts1 = np.float32([[330, 720], [448, 504], [1010, 720], [790, 504]])
+        pts2 = np.float32([[330, 720], [330, 504], [1010, 720], [1010, 504]])
+    else:
+        pts2 = np.float32([[330, 720], [448, 504], [1010, 720], [790, 504]])
+        pts1 = np.float32([[330, 720], [330, 504], [1010, 720], [1010, 504]])
     M = cv2.getPerspectiveTransform(pts1, pts2)
     image = cv2.warpPerspective(image, M, (width, height))
-    cv2.imshow('transformed', image)
+    if not reverse:
+        cv2.imshow('transformed', image)
+    else:
+        cv2.imshow('reverse transformed', image)
     return image
 
 
@@ -27,16 +34,22 @@ def roi(image):
     cv2.fillPoly(mask, rectangle, 255)
     # cv2.imshow('mask', mask)
     masked_image = cv2.bitwise_and(image, mask)
-    cv2.imshow('masked_image', masked_image)
+    # cv2.imshow('masked_image', masked_image)
     return masked_image
 
 
 def detect_edge(image):
     img = np.copy(image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    low_thres = 50
-    high_thres = 150
-    canny = cv2.Canny(gray, low_thres, high_thres)
+    kernel = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    sharp = cv2.addWeighted(gray, 1, kernel, 0, 0)
+
+    low_thres = 20
+    high_thres = 50
+    # blur = cv2.GaussianBlur(gray, (9, 9), 0)
+    canny = cv2.Canny(sharp, low_thres, high_thres)
+    cv2.imshow('edge', canny)
     return canny
 
 
@@ -49,30 +62,43 @@ def detect_line(image):
         print('No lines found!')
         return None
 
-    left_line_fit = []
-    right_line_fit = []
+    # left_line_fit = []
+    # right_line_fit = []
+    # for line in lines:
+    #     x1, y1, x2, y2 = line.squeeze()
+    #     slope, intercept = np.polyfit((x1, x2), (y1, y2), deg=1)
+    #     if slope < 0:
+    #         left_line_fit.append((slope, intercept))
+    #     else:
+    #         right_line_fit.append((slope, intercept))
+
+    # left_line_avg = np.mean(left_line_fit, axis=0) if len(
+    #     left_line_fit) else None
+    # right_line_avg = np.mean(
+    #     right_line_fit, axis=0) if len(right_line_fit) else None
+
+    left_line = []
+    right_line = []
     for line in lines:
         x1, y1, x2, y2 = line.squeeze()
-        slope, intercept = np.polyfit((x1, x2), (y1, y2), deg=1)
-        if slope < 0:
-            left_line_fit.append((slope, intercept))
+        if x1 < 640:
+            left_line.append(line.squeeze())
         else:
-            right_line_fit.append((slope, intercept))
+            right_line.append(line.squeeze())
 
-    left_line_avg = np.mean(left_line_fit, axis=0) if len(
-        left_line_fit) else None
-    right_line_avg = np.mean(
-        right_line_fit, axis=0) if len(right_line_fit) else None
-    # left_line = []
-    # right_line = []
-    # for line in left_line_fit:
-    #     left_line.append(line)
-    # for line in right_line_fit:
-    #     right_line.append(line)
+    left_line = np.mean(left_line, axis=0) if len(
+        left_line) else None
+    right_line = np.mean(right_line, axis=0) if len(right_line) else None
 
-    left_line = make_line(image, left_line_avg)
-    right_line = make_line(image, right_line_avg)
-    return np.array([left_line, right_line])
+    lines = []
+    if left_line is not None:
+        lines.append(left_line)
+    if right_line is not None:
+        lines.append(right_line)
+
+    # left_line = make_line(image, left_line_avg)
+    # right_line = make_line(image, right_line_avg)
+    return np.array(lines, dtype=np.int64)
 
 
 def make_line(image, line):
@@ -95,6 +121,7 @@ def draw_line_with_frame(frame, lines):
             x1, y1, x2, y2 = line
             cv2.line(line_img, (x1, y1), (x2, y2),
                      color=[255, 0, 0], thickness=10)
+    line_img = transform(line_img, reverse=True)
     frame = cv2.addWeighted(frame, 0.8, line_img, 1, 0)
     return frame
 
@@ -106,10 +133,10 @@ def play_video(file):
         if frame is None:
             break
         img = np.copy(frame)
-        # img = transform(img)
+        img = transform(img)
         # img = roi(img)
         img = detect_edge(img)
-        img = roi(img)
+        # img = roi(img)
         lines = detect_line(img)
         # print(lines)
         img = draw_line_with_frame(frame, lines)
