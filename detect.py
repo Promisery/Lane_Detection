f7e437677c1a757c02d652b446e12cc58e6afea0
Promisery <37978140+Promisery@users.noreply.github.com>
2020-10-25 13:57:14 +0800

    Finished! Implemented perspective transformation to the lines collected from hough transformation, so that we can calculate the average slope and intercept of the lines

--- detect.py
+++ detect.py
@@ -39,14 +39,15 @@ def roi(image):
 def detect_edge(image):
     img = np.copy(image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    print(gray.shape)
+    # print(gray.shape)
 
-    mask = (cv2.inRange(gray, np.array([0., 0., 0.517]) * 255, np.array([1., 1., 1.]) * 255) / 255).astype(np.bool)
+    mask = (cv2.inRange(gray, np.array(
+        [0., 0., 0.517]) * 255, np.array([1., 1., 1.]) * 255) / 255).astype(np.bool)
     img[np.invert(np.stack((mask, mask, mask), -1))] = 0
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # cv2.imshow('hsv', img)
-    
+
     # kernel = cv2.GaussianBlur(gray, (5, 5), 0)
 
     # sharp = cv2.addWeighted(gray, 1, kernel, 0, 0)
@@ -62,50 +63,65 @@ def detect_edge(image):
 
 def detect_line(image):
     # cv2.imshow('edge', image)
-    lines = cv2.HoughLinesP(image, 1, np.pi/180, 20,
+    lines = cv2.HoughLinesP(image, 1, np.pi/180, 100,
                             np.array([]), minLineLength=40, maxLineGap=20)
 
     if lines is None:
         print('No lines found!')
         return None
 
-    # left_line_fit = []
-    # right_line_fit = []
-    # for line in lines:
-    #     x1, y1, x2, y2 = line.squeeze()
-    #     slope, intercept = np.polyfit((x1, x2), (y1, y2), deg=1)
-    #     if slope < 0:
-    #         left_line_fit.append((slope, intercept))
-    #     else:
-    #         right_line_fit.append((slope, intercept))
-
-    # left_line_avg = np.mean(left_line_fit, axis=0) if len(
-    #     left_line_fit) else None
-    # right_line_avg = np.mean(
-    #     right_line_fit, axis=0) if len(right_line_fit) else None
-
-    left_line = []
-    right_line = []
+    left_line_fit = []
+    right_line_fit = []
+    pts1 = np.float32([[330, 720], [330, 288], [1010, 720], [1010, 288]])
+    pts2 = np.float32([[330, 720], [448, 504], [1010, 720], [790, 504]])
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    # print(M)
     for line in lines:
         x1, y1, x2, y2 = line.squeeze()
-        if x1 < 640:
-            left_line.append(line.squeeze())
+        point1 = np.array([x1, y1, 1])
+        point2 = np.array([x2, y2, 1])
+        p1 = M.dot(point1)
+        x1, y1, _ = p1 / p1[-1]
+        p2 = M.dot(point2)
+        x2, y2, _ = p2 / p2[-1]
+
+        # print(trans_points)
+        slope, intercept = np.polyfit((x1, x2), (y1, y2), deg=1)
+        # print(slope, intercept)
+        if slope < 0:
+            left_line_fit.append((slope, intercept))
         else:
-            right_line.append(line.squeeze())
+            right_line_fit.append((slope, intercept))
 
-    left_line = np.mean(left_line, axis=0) if len(
-        left_line) else None
-    right_line = np.mean(right_line, axis=0) if len(right_line) else None
+    # print(len(left_line_fit))
 
-    lines = []
-    if left_line is not None:
-        lines.append(left_line)
-    if right_line is not None:
-        lines.append(right_line)
+    left_line_avg = np.mean(left_line_fit, axis=0) if len(
+        left_line_fit) else None
+    right_line_avg = np.mean(
+        right_line_fit, axis=0) if len(right_line_fit) else None
 
-    # left_line = make_line(image, left_line_avg)
-    # right_line = make_line(image, right_line_avg)
-    return np.array(lines, dtype=np.int64)
+    # left_line = []
+    # right_line = []
+    # for line in lines:
+    #     x1, y1, x2, y2 = line.squeeze()
+    #     if x1 < 640:
+    #         left_line.append(line.squeeze())
+    #     else:
+    #         right_line.append(line.squeeze())
+
+    # left_line = np.mean(left_line, axis=0) if len(
+    #     left_line) else None
+    # right_line = np.mean(right_line, axis=0) if len(right_line) else None
+
+    # lines = []
+    # if left_line is not None:
+    #     lines.append(left_line)
+    # if right_line is not None:
+    #     lines.append(right_line)
+
+    left_line = make_line(image, left_line_avg)
+    right_line = make_line(image, right_line_avg)
+    return np.array([left_line, right_line], dtype=np.int64)
 
 
 def make_line(image, line):
@@ -125,10 +141,10 @@ def draw_line_with_frame(frame, lines):
     for line in lines:
         if line is not None:
             # print(line)
-            x1, y1, x2, y2 = line
+            x1, y1, x2, y2 = line.squeeze()
             cv2.line(line_img, (x1, y1), (x2, y2),
                      color=[255, 0, 0], thickness=10)
-    line_img = transform(line_img, reverse=True)
+    # line_img = transform(line_img, reverse=True)
     frame = cv2.addWeighted(frame, 0.8, line_img, 1, 0)
     return frame
 
